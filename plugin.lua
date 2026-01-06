5f641d9c5ea1a8499111152f6f82dc127334a666
EmeraldLockdown <86802223+EmeraldLoc@users.noreply.github.com>
2026-01-05 23:33:22 -0600

    Begin implementation of some operators. Lot more to go

--- plugin.lua
+++ plugin.lua
@@ -2,7 +2,50 @@
 function OnSetText(uri, text)
     local diffs = {}
 
+    -----------------
+    --- Operators ---
+    -----------------
+
+    -- ++V syntax
+    for start, pluses, varName, finish in text:gmatch '()(%+%+)%s*([%w_]+)()' do
+        local lineStart = text:sub(1, start):match("^.*()\n") or 1
+        diffs[#diffs+1] =  {
+            start  = start,
+            finish = start + #pluses - 1,
+            text   = '',
+        }
+
+        diffs[#diffs+1] = {
+            start  = lineStart,
+            finish = lineStart - 1,
+            text   = string.format('%s = %s + 1; ', varName, varName),
+        }
+    end
+
+    -- V++ syntax
+    for start, plusStart, finish in text:gmatch '()[%w_]+()%+%+()' do
+        diffs[#diffs+1] = {
+            start  = plusStart,
+            finish = finish,
+            text   = "",
+        }
+    end
+
+    -- ** syntax
+    --[[for start, finish in text:gmatch '%w+()%*%*()%w+' do
+        diffs[#diffs+1] = {
+            start = start,
+            finish = finish,
+            text = "^",
+        }
+    end--]]
+
+    ---------------------
+    --- Uncategorized ---
+    ---------------------
+
     -- handle v: type expressions
+    -- TODO: BUG: This code applies even in comments, which causes errors!
     for localPos, colonPos, typeName, finish in text:gmatch '()[%w_]+()%s*%:%s*([%w_]+)()' do
         diffs[#diffs+1] = {
             start  = localPos,
